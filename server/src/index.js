13279a78ff7da4a5b80e175aa7d5b5ef9545aaa1
Mohit kumar <sciencerocks8950@gmail.com>
2026-01-23 12:28:26 +0530

    set up frontend and backend

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("OK");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`your application is running on  http://localhost:${process.env.PORT}`);
+});
